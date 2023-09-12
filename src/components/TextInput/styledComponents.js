32a7a1151fd69bb28040d193d192501e4eee73ae
techanudeep74 <4a1vnp6@gmail.com>
2023-09-12 06:38:57 +0000

    EDITABLE_TEXT_INPUT

---
+++ src/components/TextInput/styledComponents.js
@@ -0,0 +1,55 @@
+import styled from 'styled-components'
+
+export const AppContainer = styled.div`
+  background-color: #f5d0fe;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+export const CardContainer = styled.div`
+  background-color: #ffffff;
+  padding: 30px;
+  height: 200px;
+  border-radius: 12px;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+export const Heading = styled.h1`
+  font-family: 'Roboto';
+  font-size: 20px;
+`
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`
+
+export const Paragraph = styled.p`
+  font-family: 'Roboto';
+  font-size: 18px;
+  padding-right: 10px;
+`
+export const Input = styled.input`
+  height: 30px;
+  width: 250px;
+  outline: none;
+  margin-right: 20px;
+  padding: 10px;
+`
+
+export const Button = styled.button`
+  height: 30px;
+  width: 55px;
+  cursor: pointer;
+  background-color: #d946ef;
+  color: #ffffff;
+  border-width: 0px;
+  font-size: 13px;
+  font-family: 'Roboto';
+`
